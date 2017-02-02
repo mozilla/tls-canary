5aa79e41052da329e3b42807338600906dbb895b
Matt Wobensmith <mwobensmith@mozilla.com>
2017-02-02 15:29:49 -0800

    Adding linux bootstrap script for AWS Ubuntu

---
+++ linux_bootstrap.sh
@@ -0,0 +1,9 @@
+# AWS Ubuntu linux bootstrap
+sudo apt-get update
+sudo apt-get install libgtk-3-0
+sudo apt-get install libxt6
+sudo apt-get install libasound2
+sudo apt-get install python
+sudo apt-get install python-dev
+sudo apt-get install virtualenv
+sudo apt-get install gcc
