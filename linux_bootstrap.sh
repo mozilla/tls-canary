016fb66fb10c07dc1e3f5950c5dade6888eb0013
Matt Wobensmith <mwobensmith@mozilla.com>
2017-03-15 17:16:19 -0700

    Fixing problem with permissions on Ubuntu Linux

--- linux_bootstrap.sh
+++ linux_bootstrap.sh
@@ -9,3 +9,4 @@ sudo apt-get install python-dev
 sudo apt-get install virtualenv
 sudo apt-get install gcc
 sudo apt-get install golang-go
+cd $(dirname $(which go)) && cd $(dirname $(readlink go)) && cd ../ && sudo chmod 0777 ./src
