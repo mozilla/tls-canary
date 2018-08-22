19b7e86458fc42c7ec3d2ad04cb07e4d69ea4ced
Christiane Ruetten <cr@mozilla.com>
2018-08-22 14:40:00 +0200

    Moving to go 1.9

--- bootstrap/linux_bootstrap.sh
+++ bootstrap/linux_bootstrap.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get -y install \
     gcc \
-    golang-go \
+    golang-1.9-go \
     libasound2 \
     libffi-dev \
     libgtk-3-0 \
@@ -19,3 +19,9 @@ sudo apt-get -y install virtualenv \
 	|| sudo apt-get -y install python-virtualenv
 
 sudo apt-get remove python-six  # Native six module causes version conflict
+
+# Fix go environment for using go 1.9
+sudo update-alternatives --remove-all go
+sudo update-alternatives --remove-all gofmt
+sudo update-alternatives --install /usr/bin/go go /usr/lib/go-1.9/bin/go 20
+sudo update-alternatives --install /usr/bin/gofmt gofmt /usr/lib/go-1.9/bin/gofmt 20
