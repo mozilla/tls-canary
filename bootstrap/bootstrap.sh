fe3cb853717114f318ecd9483866e0fdd01c7d7a
Christiane Ruetten <cr@mozilla.com>
2018-09-27 14:19:24 +0200

    Adding bootstrap for Arch Linux / pacman

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,6 +4,11 @@ then
 	echo "Bootstrapping for Linux / apt-get"
 	echo "sudo required"
 	sudo $(dirname "$0")/linux_bootstrap.sh
+elif which pacman &>/dev/null
+then
+	echo "Bootstrapping for Arch Linux / pacman"
+	echo "sudo required"
+	sudo $(dirname "$0")/arch_bootstrap.sh
 elif which brew &>/dev/null
 then
 	echo "Bootstrapping for Mac OS X / Homebrew"
