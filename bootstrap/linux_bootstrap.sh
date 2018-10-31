2461d753c81a93c9aa649246ee07ff8cb130f134
Christiane Ruetten <cr@mozilla.com>
2018-10-31 16:27:17 +0100

    Adjusting for AWS Ubuntu 18.4

--- bootstrap/linux_bootstrap.sh
+++ bootstrap/linux_bootstrap.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-# AWS Ubuntu linux bootstrap
+# AWS Ubuntu 18.4 linux bootstrap
 sudo apt-get update
 sudo apt-get -y install \
     gcc \
-    golang-1.9-go \
+    golang-go \
     libasound2 \
+    libdbus-glib-1-2 \
     libffi-dev \
     libgtk-3-0 \
     libssl-dev \
     libxt6 \
     p7zip-full \
-    python \
-    python-dev \
-    python-pip
+    python3 \
+    python3-dev \
+    python3-pip
 
 # The virtualenv package is not consistently named across distros
 sudo apt-get -y install virtualenv \
-	|| sudo apt-get -y install python-virtualenv
+    || sudo apt-get -y install python3-virtualenv
 
-sudo apt-get remove python-six  # Native six module causes version conflict
-
-# Fix go environment for using go 1.9
-sudo update-alternatives --remove-all go
-sudo update-alternatives --remove-all gofmt
-sudo update-alternatives --install /usr/bin/go go /usr/lib/go-1.9/bin/go 20
-sudo update-alternatives --install /usr/bin/gofmt gofmt /usr/lib/go-1.9/bin/gofmt 20
