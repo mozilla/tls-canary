2aee07e3bd4c0f23d56cf24fef20d877a2d341cf
christopher-henderson <chris@chenderson.org>
2019-11-29 13:53:50 -0700

    starting work on cert_storage writer

--- tlscanary/tools/one_crl_downloader.py
+++ tlscanary/tools/one_crl_downloader.py
@@ -95,3 +95,57 @@ def get_list(onecrl_env, workdir, commit, use_cache=True, cache_timeout=60*60):
         logger.warning("Using cached OneCRL revocations data from `%s`" % cache_file)
 
     return cache_file
+
+# populate_cert_storage creates an LMDB database populated with the contents of the provided
+# OneCRL environment (one of either "stage" or "production"). The created database will be
+# located at <WORKDIR>/cache/<ONE_CRL_ENV>_cert_storage/security/data.safe.bin. This database
+# should in turn be copied to <FIREFOX_PROFILE>/security_state/data.safe.bin.
+def populate_cert_storage(onecrl_env, workdir, commit, use_cache=True, cache_timeout=60*60):
+
+    # @TODO PATH needs `/.cargo/bin
+    global logger
+
+    dc = cache.DiskCache(os.path.join(workdir, "cache"), cache_timeout, purge=True)
+    cache_id = "%s_cert_storage" % onecrl_env
+    if not use_cache:
+        # Enforce re-extraction even if cached
+        dc.delete(cache_id)
+    if cache_id in dc:
+        logger.warning("Using cached OneCRL cert_storage data from `%s`" % cached_security_state)
+        return
+
+    cached_security_state = dc[cache_id]
+    os.makedirs(cached_security_state)
+    # @TODO
+    # What should this be? Above, we were essentially relying on the GOPATH as
+    # a default place to have a cloned repo.
+    repo_dir = "/Users/chris/Documents/Contracting/mozilla/OneCRL-Tools"
+
+    os.putenv("")
+    cargo_bin = find_executable("cargo")
+    if cargo_bin is None:
+        logger.critical("Cannot find Cargo toolchain")
+        sys.exit(5)
+    logger.debug("Using Cargo toolchain at `%s`" % cargo_bin)
+
+    # Checkout a known-working commit before running
+    logger.debug("Checking out commit `%s` in `%s`" % (commit, repo_dir))
+    if subprocess.call(["git", "checkout", "-q", commit], cwd=repo_dir) != 0:
+        logger.critical("Cannot checkout OneCRL-Tools git commit `%s`" % commit)
+        sys.exit(5)
+    tool_dir = os.path.join(repo_dir, "one_crl_to_cert_storage")
+    # The user may have their global toolchain set to nightly, but we would
+    # like if the local use of the toolchain pointed to stable.
+    if subprocess.call(["rustup", "override", "set", "--path", tool_dir, "stable"]) != 0:
+        logger.critical("Cannot set the working toolchain for `%s` to stable" % tool_dir)
+        sys.exit(5)
+    # cargo run --manifest-path one_crl_to_cert_storage/Cargo.toml -- --env $onecrl_env --profile $profile_path
+    manifest = os.path.join(tool_dir, "Cargo.toml")
+    result = subprocess.call([
+        # "--" delimits arguments given to Cargo from the arguments given to the built tool.
+        "cargo", "run", "--manifest-path", manifest, "--", 
+        # arguments for the tool itself
+        "--env", onecrl_env, "--profile", cached_security_state])
+    if result != 0:
+        logger.critical("Cannot populate cert_storage from OneCRL")
+        sys.exit(5)
